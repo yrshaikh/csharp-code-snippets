bb4c32d436e476d101eb17b353079b137ac5c507
Yasser Shaikh <yasser.shaikh@agoda.com>
2021-03-29 07:43:44 +0700

    feat(extensions): added templates and sample code for extensions.

---
+++ src/Snippets/Snippets.UnitTests/Extensions/StringExtensionsTest.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+using Shouldly;
+using Snippets.Core.Extensions;
+
+namespace Snippets.UnitTests.Extensions
+{
+    [TestFixture]
+    public class StringExtensionsTest
+    {
+        [Test]
+        [TestCase("hello", 5)]
+        [TestCase("good morning", 12)]
+        public void AppendZzz_ShouldWorkCorrectly(string input, int expectedWordCount)
+        {
+            input.GetWordCount().ShouldBe(expectedWordCount);
+        }
+    }
+}
